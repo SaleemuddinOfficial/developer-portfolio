5f7cf880c01e9a640a96e17e1a0055d565cf3b5b
Saleemuddin Khan <saleem773@gmail.com>
2024-12-13 16:56:33 +0530

    Profile Details Updated

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -1,20 +1,20 @@
 export const educations = [
   {
     id: 1,
-    title: "Bachelor Degree",
-    duration: "2020 - Present",
-    institution: "National University of Bangladesh",
+    title: "Bachelor Degree - Computer Science",
+    duration: "2013 - 2017 ",
+    institution: "Sanpada College of Commerce and Technology",
   },
   {
     id: 2,
     title: "Higher Secondary Certificate",
-    duration: "2018 - 2020",
-    institution: "Noakhali Islamia Kamil Madrasah",
+    duration: "2011 - 2013",
+    institution: "Sanpada College of Commerce and Technology",
   },
   {
     id: 3,
     title: "Secondary School Certificate",
-    duration: "2008 - 2018",
-    institution: "Baitus Saif Islamia Madrasah",
+    duration: "2010 - 2011",
+    institution: "Anjuman Tablighul Islam Urdu High School",
   }
 ]
