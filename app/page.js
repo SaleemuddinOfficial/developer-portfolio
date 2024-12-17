36d73b6a0734a2d75a8aab94cc3a3744266a7e16
Saleemuddin Khan <saleem773@gmail.com>
2024-12-17 11:25:23 +0530

    minor fix

--- app/page.js
+++ app/page.js
@@ -1,5 +1,13 @@
 import dynamic from 'next/dynamic';
 import { personalData } from "@/utils/data/personal-data";
+import Blog from "./components/homepage/blog";
+const HeroSection = dynamic(() => import("./components/homepage/hero-section"), { ssr: false });
+const AboutSection = dynamic(() => import("./components/homepage/about"), { ssr: false });
+const Experience = dynamic(() => import("./components/homepage/experience"), { ssr: false });
+const Skills = dynamic(() => import("./components/homepage/skills"), { ssr: false });
+const Projects = dynamic(() => import("./components/homepage/projects"), { ssr: false });
+const Education = dynamic(() => import("./components/homepage/education"), { ssr: false });
+const ContactSection = dynamic(() => import("./components/homepage/contact"), { ssr: false });
 // import AboutSection from "./components/homepage/about";
 // import Blog from "./components/homepage/blog";
 // import ContactSection from "./components/homepage/contact";
@@ -9,13 +17,7 @@ import { personalData } from "@/utils/data/personal-data";
 // import Projects from "./components/homepage/projects";
 // import Skills from "./components/homepage/skills";
 
-const HeroSection = dynamic(() => import("./components/homepage/hero-section"), { ssr: false });
-const AboutSection = dynamic(() => import("./components/homepage/about"), { ssr: false });
-const Experience = dynamic(() => import("./components/homepage/experience"), { ssr: false });
-const Skills = dynamic(() => import("./components/homepage/skills"), { ssr: false });
-const Projects = dynamic(() => import("./components/homepage/projects"), { ssr: false });
-const Education = dynamic(() => import("./components/homepage/education"), { ssr: false });
-const ContactSection = dynamic(() => import("./components/homepage/contact"), { ssr: false });
+
 
 async function getData() {
   const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`)
